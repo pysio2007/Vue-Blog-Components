3fec24dd3fd7cbf1637dec7ba030fc0ddc925ea5
Pysio <qq593277393@outlook.com>
2024-11-07 00:02:23 +0800

    Init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import GithubIssues from "./GithubIssues.vue";
+import GitHubPRList from "./GitHubPRList.vue";
+import AsciinemaPlayer from "./AsciinemaPlayer.vue";
+import Fastfetch from "./Fastfetch.vue";
+import OnlineOrDead from "./OnlineOrDead.vue";
+import SteamStatus from "./SteamStatus.vue";
+import HuoShen80Hub from "./HuoShen80Hub.vue";
+import Wordle from "./Wordle.vue";
+import console from "./console.vue";
+import ipcheck from "./ipcheck.vue";
+import Connectivity from "./Connectivity.vue";
+import WebRTC from "./WebRTC.vue";
+
+export {
+    GithubIssues,
+    GitHubPRList,
+    AsciinemaPlayer,
+    Fastfetch,
+    OnlineOrDead,
+    SteamStatus,
+    HuoShen80Hub,
+    Wordle,
+    console,
+    ipcheck,
+    Connectivity,
+    WebRTC,
+};
